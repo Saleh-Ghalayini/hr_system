f0cef4ef8674578f205a4f6307dab75219b4e437
Hasanmawasi <hasanhasann@gmail.com>
2025-03-20 13:59:35 +0200

    navbar styles

---
+++ hr-system-frontend/src/components/NavBar/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+import './style.css';
+const NavBar = () => {
+    
+    return (
+        <nav>
+        
+            <div className="title">Title</div>
+
+            <div className='profile-section'>
+            <Icon className='icons' icon="bi:bell-fill" width="20" height="20" />
+            <Icon className='icons' icon="ic:baseline-settings" width="24" height="24" />
+            <img src="logo.png" alt="Profile picture" onClick={()=>console.log("progile")} />            
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
