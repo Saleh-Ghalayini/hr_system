c2ab0704a03b51faceea1e6f3c46d5046aa83072
mahditalaldev <mahditalal.789@gmail.com>
2025-03-20 17:14:23 +0200

    created the Table component make it reusable

---
+++ hr-system-frontend/src/components/Table/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+// import PropTypes from 'prop-types';
+import styles from "./Table.module.css";
+import StatusField from "../StatusField";
+
+const Table = ({ headers, data }) => {
+  return (
+    <div className={styles.tableContainer}>
+      <table className={styles.dataTable}>
+        <thead>
+          <tr>
+            {headers.map((header) => (
+              <th key={header.key} className={styles.tableHeader}>
+                {header.label}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index} className={styles.tableRow}>
+              {headers.map((header) =>
+                header.key === "status" ? (
+                  <td key={header.key} className={styles.tableCell}>
+                    <StatusField
+                      text={row[header.key]}
+                      status={row[header.key]}
+                    />
+                    {/* {row[header.key]} */}
+                  </td>
+                ) : (
+                  <td key={header.key} className={styles.tableCell}>
+                    {row[header.key]}
+                  </td>
+                )
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
