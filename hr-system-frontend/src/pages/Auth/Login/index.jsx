0fbe362c8f52f8a06234fc2f9a82bd84e1a1fa2c
saleh <ghalayinisaleh9@gmail.com>
2025-03-22 08:12:09 +0200

    created the design of the login page

--- hr-system-frontend/src/pages/Auth/Login/index.jsx
+++ hr-system-frontend/src/pages/Auth/Login/index.jsx
@@ -1,37 +1,51 @@
 import React, { useState } from "react";
-// import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../../../context/AuthContext";
-// import Table from "../../../components/Table";
+import Button from "../../../components/Button";
+import Input from "../../../components/Input";
+import "./style.css";
 
 const Login = () => {
-   
-   
   const { login } = useAuthContext();
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     login(email, password);
   };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
+    <div className="wrapper">
+      <div className="container login">
+      <h1 className="blue">Login</h1>
+
+        <Input
           type="text"
-          placeholder="Email"
           value={email}
+          label="Email"
+          className= "input label"
+          placeholder="Example@gmail.com"
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
+
+        <Input
           type="password"
-          placeholder="Password"
           value={password}
+          label="Password"
+          className= "input label"
+          placeholder="User@134"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Login</button>
-      </form>
+
+      <div className="remember-forgot small-text">
+        <div className="remember">
+          <label className="blue"><input type="checkbox" className="checkbox" />Remember me</label>
+        </div>
+        <a className="blue" href="#">Forgot password?</a>
+      </div>
+
+      <Button className="login-btn" text="Log in" onClick={handleSubmit} />
+    </div>
     </div>
   );
 };
