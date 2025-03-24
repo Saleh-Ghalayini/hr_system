a259551b85495777f13233000c27f17e77684284
mahditalaldev <mahditalal.789@gmail.com>
2025-03-24 02:09:33 +0200

    created the migrations table needed for leaves feature

--- hr-system-backend/database/migrations/2025_03_24_000240_create_leave_types_table.php
+++ hr-system-backend/database/migrations/2025_03_24_000240_create_leave_types_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('task_dependencies', function (Blueprint $table) {
+        Schema::create('leave_types', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('task_id')->constrained()->onDelete('cascade');
-            $table->foreignId('depends_on_task_id')->constrained('tasks')->onDelete('cascade');
+            $table->enum('type', ['annual', 'sick', 'casual', 'other']);
+            $table->integer('max_absence')->default(15);
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('task_dependencies');
+        Schema::dropIfExists('leave_types');
     }
 };
