3b08bb528338b53b847169ad53d01763c7e50163
CaptainSlash44 <michaelnjeim44@gmail.com>
2025-03-25 18:46:16 +0200

    implemented changing insurance feature

--- hr-system-backend/app/Http/Controllers/InsuranceController.php
+++ hr-system-backend/app/Http/Controllers/InsuranceController.php
@@ -10,4 +10,17 @@ class InsuranceController extends Controller
     public function getInsurances(){
         return Insurance::all();
     }
+
+    public function updatePlan(Request $request){
+        $new_value = $request["value"];
+        $insurance_type = $request["type"];
+        $insurance = Insurance::where('type', $insurance_type)->first();
+        $insurance->cost = $new_value;
+        $insurance->save();
+
+        return response()->json([
+            "success" => true,
+            "message" => "updated successfully",
+        ]);
+    }
 }
