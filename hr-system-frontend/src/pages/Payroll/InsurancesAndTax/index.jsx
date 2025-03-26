f882104220e8d024fb5759c6439889573a8a09c6
CaptainSlash44 <michaelnjeim44@gmail.com>
2025-03-26 10:50:08 +0200

    fixed state updating on the insurance card updater

--- hr-system-frontend/src/pages/Payroll/InsurancesAndTax/index.jsx
+++ hr-system-frontend/src/pages/Payroll/InsurancesAndTax/index.jsx
@@ -1,26 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InsuranceCard from "../../../components/InsuranceCard";
+import { request } from "../../../common/request";
+import InsuranceUpdater from "../../../components/InsuranceUpdater";
+import Button from "../../../components/Button";
 
 const InsuranceAndTax = ({}) => {
-  const insurances = [
-    {
-      type: "HAF",
-      cost: "50",
-    },
-    {
-      type: "GNA",
-      cost: "70",
-    },
-  ];
+  const [loading, setLoading] = useState(true);
+  const [insuranceData, setInsuranceData] = useState();
+  const [trigger, setTrigger] = useState(false);
+  const [insuranceType, setInsuranceType] = useState();
+  const [value, setValue] = useState();
 
-  return (
+  useEffect(() => {
+    const fetchInsurance = async () => {
+      try {
+        setLoading(true);
+        const response = await request({
+          method: "GET",
+          path: "admin/getinsurances",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        setInsuranceData(response);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchInsurance();
+  }, []);
+
+  const updateInsurance = async () => {
+    const response = await request({
+      method: "POST",
+      path: "admin/updateinsurance",
+      data: {
+        type: insuranceType,
+        value: value,
+      },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    if (response.success) {
+      setTrigger(!trigger);
+      window.location.reload();
+    }
+  };
+
+  return loading ? (
+    <h1>Loading</h1>
+  ) : (
     <div className="flex">
-      {insurances.map((insurance) => (
+      {insuranceData.map((insurance) => (
         <InsuranceCard
           insuranceType={insurance.type}
           deduction={insurance.cost}
+          onClick={() => {
+            setTrigger(!trigger);
+            setInsuranceType(insurance.type);
+          }}
         />
       ))}
+      <div>
+        {trigger && (
+          <div className="updater">
+            <div className="updater-container flex flex-dir-col align-center border-rad-six">
+              <header>
+                <h2>{insuranceType}</h2>
+              </header>
+              <input
+                type="text"
+                className="update-input"
+                onChange={(e) => {
+                  setValue(e.target.value);
+                  console.log(value);
+                }}
+              />
+              <Button
+                text="UPDATE"
+                className="primary-btn"
+                onClick={() => {
+                  updateInsurance();
+                  setTrigger(!trigger);
+                }}
+              />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
